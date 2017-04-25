e3c637ccc48ec477f6f39b6c3fcca0606ff43002
Miguel Angel Ajo <mangelajo@redhat.com>
2017-04-25 08:47:39 -0400

    Use master branch for ovs/ovn

--- scripts/rhel-ovn.sh
+++ scripts/rhel-ovn.sh
@@ -1,6 +1,11 @@
 # Add a repo for where we can get OVS 2.6 packages
-if [ ! -f /etc/yum.repos.d/delorean-deps.repo ] ; then
-    curl http://trunk.rdoproject.org/centos7/delorean-deps.repo | sudo tee /etc/yum.repos.d/delorean-deps.repo
+#if [ ! -f /etc/yum.repos.d/delorean-deps.repo ] ; then
+#    curl http://trunk.rdoproject.org/centos7/delorean-deps.repo | sudo tee /etc/yum.repos.d/delorean-deps.repo
+#fi
+
+# setup OVS master too
+if [ ! -f /etc/yum.repos.d/ovsmaster.repo ] ; then
+    curl https://copr.fedorainfracloud.org/coprs/leifmadsen/ovs-master/repo/epel-7/leifmadsen-ovs-master-epel-7.repo | sudo tee /etc/yum.repos.d/ovsmaster.repo
 fi
 
 sudo yum install -y
@@ -8,7 +13,8 @@ sudo yum remove -y firewalld
 
 /vagrant/scripts/install-netperf.sh
 
-sudo yum install -y epel-release git git-review python-pip gcc python-devel \
+sudo yum install -y --nogpgcheck  \
+                    epel-release git git-review python-pip gcc python-devel \
                     openvswitch openvswitch-ovn-central openvswitch-ovn-host
 sudo setenforce 0
 
@@ -27,7 +33,9 @@ done
 # on gw1 we run ovn-northd
 
 if [[ "$hostname" == "gw1" ]]; then
-    
+    cat | sudo tee /etc/sysconfig/ovn-northd <<EOF
+OVN_NORTHD_OPTS="--db-sb-create-insecure-remote=yes --db-nb-create-insecure-remote=yes"
+EOF
     sudo systemctl enable ovn-northd
     sudo systemctl start ovn-northd
     sudo systemctl status ovn-northd
