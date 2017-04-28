7d7355658d673b183682db8b7ebe922752ae7e1d
Miguel Angel Ajo <mangelajo@redhat.com>
2017-04-28 05:48:29 -0400

    Compile OVN/OVS from sources and setup system-id

--- scripts/rhel-ovn.sh
+++ scripts/rhel-ovn.sh
@@ -15,8 +15,21 @@ sudo yum remove -y firewalld
 
 sudo yum install -y --nogpgcheck  \
                     epel-release git git-review python-pip gcc python-devel \
-                    openvswitch openvswitch-ovn-central openvswitch-ovn-host
+                    openvswitch openvswitch-ovn-central openvswitch-ovn-host \
+                    make autoconf openssl-devel automake python-devel \
+                    kernel-devel graphviz kernel-debug-devel rpm-build \
+                    redhat-rpm-config libtool checkpolicy selinux-policy-devel \
+                    python-six vim
+
 sudo setenforce 0
+
+
+git clone https://github.com/openvswitch/ovs
+cd ovs
+./boot.sh
+./configure --prefix=/
+make -j5 V=0 install
+sudo make install
 
 # lookup my hostname IP from env
 hostname=$(hostname)
@@ -30,8 +43,9 @@ for n in openvswitch ovn-controller ; do
     systemctl status $n
 done
 
-# on gw1 we run ovn-northd
-
+#
+# on gw1 we run the ovn-northd controller
+#
 if [[ "$hostname" == "gw1" ]]; then
     cat | sudo tee /etc/sysconfig/ovn-northd <<EOF
 OVN_NORTHD_OPTS="--db-sb-create-insecure-remote=yes --db-nb-create-insecure-remote=yes"
@@ -41,8 +55,17 @@ EOF
     sudo systemctl status ovn-northd
 fi
 
+sleep 2
+
+sudo ovs-vsctl set open . external-ids:ovn-bridge=br-int
+sudo ovs-vsctl set open . external-ids:system-id=$hostname
 sudo ovs-vsctl set open . external-ids:ovn-remote=tcp:${gw1}:6642
 sudo ovs-vsctl set open . external-ids:ovn-encap-type=geneve
 sudo ovs-vsctl set open . external-ids:ovn-encap-ip=$ip
+
+sleep 2
+
+sudo ovs-vsctl list open
+
 
 exit 0
