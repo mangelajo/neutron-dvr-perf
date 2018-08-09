6e6d732cd5df889799a6f76a025f1f46b651566e
Miguel Angel Ajo Pelayo <majopela@redhat.com>
2018-08-09 07:52:23 +0000

    Workaround issues with setuptools in centos

--- lib/scripts/rhel-devstack.sh
+++ lib/scripts/rhel-devstack.sh
@@ -1,6 +1,12 @@
+sudo yum install -y https://www.rdoproject.org/repos/rdo-release.rpm
+sudo yum update -y
+
 sudo yum install -y epel-release git git-review python-pip gcc python-devel
 sudo yum remove -y firewalld
 sudo pip install --upgrade pip
+sudo yum remove -y python-setuptools
+sudo yum install -y python-setuptools
+sudo pip install psutil
 
 /vagrant/install-netperf.sh
 
